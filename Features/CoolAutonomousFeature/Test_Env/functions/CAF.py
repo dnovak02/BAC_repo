7bb7cd6116ed5afb0fe4d4fae0dfd09c35738baa
dnovak02 <novak.antal.david@gmail.com>
2022-11-29 22:30:07 +0100

    baseline CAF function

---
+++ Features/CoolAutonomousFeature/Test_Env/functions/CAF.py
@@ -0,0 +1,14 @@
+#!/bin/bash/python3
+
+
+class CAF():
+    
+    def __init__(self,json):
+        pass
+    
+    def json_fill(self):
+        self
+        pass
+    
+    def set_CAF_is_Active(self):
+        pass
